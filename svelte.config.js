f4f26c606432c4cf63c5a5727f5e79702c9c027c
acsoliz <acsoliz@outlook.com>
2024-05-31 02:44:10 +0200

    fixing to debug

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,22 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-cloudflare';
-// import adapter from '@sveltejs/adapter-static';
+
+// import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-static';
+
 // 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter(),
+		// adapter: adapter(),
+		paths: { assets: "", base: "/PERSONAL_PATH" },
+		adapter: adapter({
+			// NOT HERE!
+			paths: { base: "/alerts-app" },
+			fallback: 'index.html',
+			precompress: false,
+		}),
 		// adapter: adapter({
 		// 	pages: 'build',
 		// 	assets: 'build',
