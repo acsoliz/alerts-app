99e06a3ede9c85897c2ab9172bc5134dcc56a0b4
acsoliz <acsoliz@outlook.com>
2024-06-01 23:07:38 +0200

    fixing error to deploy on cloduflare. commented component register

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,16 +1,24 @@
 <script lang="ts">
   import { staticState } from "$lib";
   import { onMount } from "svelte";
-  import Login from "./login.svelte";
-  import Register from "./register.svelte";
+  // import Login from "./login.svelte";
+  import Register from "../login/register.svelte";
+  import Login from "../login/login.svelte";
+  // import Register from "./register.svelte";
+
   import { getSession } from "../../hooks.client";
   import { goto } from "$app/navigation";
 
   onMount(async () => {
-    if (await getSession()) {
+    const session = await getSession();
+    console.log("Delete to deploy. Session:", session);
+    if (session) {
       goto("/");
     }
   });
+  console.log("Register:: ", Register);
+  console.log("Login:: ", Login);
+  console.log("staticState:: ", staticState);
 </script>
 
 {#await getSession()}
