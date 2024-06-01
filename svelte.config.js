9f0ce381ae56d402343c2e45c17d9809770d5862
acsoliz <acsoliz@outlook.com>
2024-06-01 22:27:37 +0200

    fixing to deploy, svelte.config updated, default configrs, tested build & preview in local

--- svelte.config.js
+++ svelte.config.js
@@ -4,26 +4,13 @@ import adapter from '@sveltejs/adapter-cloudflare';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
-
-	// kit: {
-	// 	adapter: adapter(),
 	// 	prerender: {
 	// 		entries: ['*']
-	// 	},
-	// 	alias: {
-	// 		"@/*": "./src/*",
-	// 	},
-	// }
 	kit: {
 		adapter: adapter(),
-		target: '#svelte',
-		vite: {
-			resolve: {
-				alias: {
-					$routes: '/src/routes'
-				}
-			}
-		}
+		alias: {
+			"@/*": "./src/*",
+		},
 	}
 };
 
